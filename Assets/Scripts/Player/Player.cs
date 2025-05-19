43193acfdb9de89229757a0cdf5dee50ed32dde1
ALeving <fgileving@gmail.com>
2025-05-19 22:29:30 +0300

    finish base movement

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,14 +1,44 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Player
 {
     public class Player : MonoBehaviour
     {
         [SerializeField] private float movementSpeed = 5f;
-    
+        [SerializeField] private float horizontalSpeed = 5f;
+        [SerializeField] private Rigidbody rb;
+        
+        private float _horizontalVelocity;
+        private Keyboard _keyboard;
+
+        private void Awake()
+        {
+            _keyboard = Keyboard.current;
+        }
+
         private void Update()
         {
-            transform.Translate(Vector3.forward * (movementSpeed * Time.deltaTime));
+            UpdateForwardMovement();
+            
+            _horizontalVelocity = _keyboard.dKey.isPressed ? horizontalSpeed : 
+                _keyboard.aKey.isPressed ? -horizontalSpeed : 0f;
+
+        }
+
+        private void UpdateForwardMovement()
+        {
+            //transform.Translate(Vector3.forward * (movementSpeed * Time.deltaTime));
+            
+            transform.Translate(new Vector3(_horizontalVelocity, 0f, movementSpeed) * Time.deltaTime);
+        }
+        
+        private void FixedUpdate()
+        {
+            //rb.AddForce(new Vector3(_horizontalVelocity, 0f, 0f), ForceMode.VelocityChange);
+            
+            //rb.MovePosition(rb.position + new Vector3(_horizontalVelocity, 0f, movementSpeed) * Time.fixedDeltaTime);;
         }
     }
 }
